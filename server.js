91b55ca7c704d2c94d14051c5724b17c61c1080a
SITRUK <kurtis.santillanes@gmail.com>
2019-05-23 07:46:46 -0700

    repurposing the star wars exercise to model npm packages utilized

---
+++ server.js
@@ -0,0 +1,94 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Friends (DATA)
+// =============================================================
+var person = [
+  {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  },
+  {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+  {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Displays all people
+app.get("/api/person", function(req, res) {
+  return res.json(person);
+});
+
+// Displays a single person, or returns false
+app.get("/api/person/:friend", function(req, res) {
+  var chosen = req.params.friend;
+
+  console.log(chosen);
+
+  for (var i = 0; i < person.length; i++) {
+    if (chosen === person[i].routeName) {
+      return res.json(person[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New People - takes in JSON input
+app.post("/api/person", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newcharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  person.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
