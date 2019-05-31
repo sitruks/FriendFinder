d2537e768cab7a76642b45b9069e57781c3a4800
SITRUK <kurtis.santillanes@gmail.com>
2019-05-30 17:46:11 -0700

    adding index page from example as reference for test.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,16 @@
 //Use router to create router module
-var express = require('express');
-var potentialFriends = require('../data/friends.js');
-var router = express.Router();
+const express = require('express');
+const potentialFriends = require('../data/friends');
+const router = express.Router();
 
 // GET request to display all potential friends in JSON format with /api/friends entered as end point in url
-router.get("/api/friends", function (req, res) {
+router.get('/api/friends', function (req, res) {
     return res.json(potentialFriends);
 });
 
 // POST request to receive data from client and push to potentialFriends array
-router.post("/api/friends", function (req, res) {
+router.post('/api/friends', function (req, res) {
     return potentialFriends.push(req.body);
 });
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
-var api = require('./app/routing/apiRoutes');
-var html = require('./app/routing/htmlRoutes');
+const express = require("express");
+const path = require("path");
+const htmlRoute = require('./app/routing/htmlRoutes');
+const apiRoute = require('./app/routing/apiRoutes');
 
 // Set up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8000;
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
-api.use(api);
-app.use(html);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/', htmlRoute);
+app.use('/api', apiRoute);
 
 // Starts the server to begin listening
 // =============================================================
